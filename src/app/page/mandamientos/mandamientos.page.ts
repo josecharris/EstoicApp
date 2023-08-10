b6554f029704fab23a08617d86e2d0ac514d5113
josecharris <josepthc9781@gmail.com>
2023-08-10 13:22:31 -0500

    Se agrega botón volver en la pantalla de mandamientos

--- src/app/page/mandamientos/mandamientos.page.ts
+++ src/app/page/mandamientos/mandamientos.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-mandamientos',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MandamientosPage implements OnInit {
 
-  constructor() { }
+  constructor( private router: Router ) { }
 
   ngOnInit() {
+  }
+
+  public volver(): void{
+    this.router.navigate(['/tab-inicial/conceptos']);
   }
 
 }
