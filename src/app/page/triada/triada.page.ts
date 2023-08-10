6de89c2f6c2480d5c981f59518f6dbdf93910d4d
josecharris <josepthc9781@gmail.com>
2023-08-10 13:13:31 -0500

    Se agrega botón volver desde la triada a la pantalla de conceptos

--- src/app/page/triada/triada.page.ts
+++ src/app/page/triada/triada.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-triada',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TriadaPage implements OnInit {
 
-  constructor() { }
+  constructor( private router: Router ) { }
 
   ngOnInit() {
+  }
+
+  public volver(): void{
+    this.router.navigate(['/tab-inicial/conceptos']);
   }
 
 }
