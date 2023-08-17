38c5cd7df8dba34531073655ced6e0a04243af48
josecharris <josepthc9781@gmail.com>
2023-08-16 21:23:59 -0500

    Se agregan los primeros 8 títulos de lecturas

--- src/app/page/lecturas/lecturas.page.ts
+++ src/app/page/lecturas/lecturas.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lecturas',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LecturasPage implements OnInit {
 
-  constructor() { }
+  constructor( private router: Router ) { }
 
   ngOnInit() {
+  }
+
+  public navegarMeditaciones(): void {
+    this.router.navigate(['/tab-inicial/conceptos']);
   }
 
 }
