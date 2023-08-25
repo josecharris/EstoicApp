00bbba2d456e46cd6f44e72ae967139f4479215f
josecharris <josepthc9781@gmail.com>
2023-08-25 15:19:30 -0500

    Se agrega contenido de imagenes

--- src/app/page/frases/frases.page.ts
+++ src/app/page/frases/frases.page.ts
@@ -12,12 +12,14 @@ export class FrasesPage implements OnInit {
     {
       frase: "No busques que los acontecimientos sucedan como deseas; desea que los acontecimientos sucedan como suceden, y serás feliz.",
       autor: "EPICTETO",
-      explicacion: "Aceptar la realidad tal como es, sin resistencia, nos permite encontrar la paz interior y ser más felices con lo que tenemos."
+      explicacion: "Aceptar la realidad tal como es, sin resistencia, nos permite encontrar la paz interior y ser más felices con lo que tenemos.",
+      image: "./../../../assets/images/pensando.jpg"
     },
     {
       frase: "No son las cosas en sí mismas las que nos perturban, sino nuestra interpretación de ellas.",
       autor: "EPICTETO",
-      explicacion: "Nuestras emociones están influenciadas por cómo interpretamos las situaciones. Elegir interpretaciones sabias y serenas nos ayuda a mantener la calma."
+      explicacion: "Nuestras emociones están influenciadas por cómo interpretamos las situaciones. Elegir interpretaciones sabias y serenas nos ayuda a mantener la calma.",
+      image: "./../../../assets/images/color.avif"
     },
   ];
   constructor() { }
