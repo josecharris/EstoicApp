49bdc2ec8d5b8cdcf1dc4ea4a93664981c98b2dc
josecharris <josepthc9781@gmail.com>
2023-08-26 11:08:34 -0500

    Se automatiza el cargue aleatorio de imagenes

--- src/app/dto/frase.dto.ts
+++ src/app/dto/frase.dto.ts
@@ -3,9 +3,10 @@ export class Frase{
     public autor: string = "";
     public explicacion: string = "";
     public image: string = "";
-    constructor(frase: string, autor: string, explicacion: string){
+    constructor(frase: string, autor: string, explicacion: string, image: string){
         this.frase = frase;
         this.autor = autor;
         this.explicacion = explicacion;
+        this.image = image;
     }
 }
--- src/app/page/frases/frases.page.ts
+++ src/app/page/frases/frases.page.ts
@@ -13,15 +13,17 @@ export class FrasesPage implements OnInit {
 
   public lstFrases = jsonFile.frases;
   public frases: Frase[] = [];
+  public asset: string = "./../../../assets/images/";
+  public imagenes: string[] = [];
 
   constructor() {
+    this.cargarImagenes();
     this.cargarJSON();
   }
 
   ngOnInit() {
     const mySwiper = new Swiper('.swiper-container', {
       direction: 'vertical',
-      // Otras opciones de configuración aquí
     });
   }
 
@@ -30,9 +32,24 @@ export class FrasesPage implements OnInit {
       const descFrase = frase.frase;
       const autor = frase.autor;
       const explicacion = frase.explicacion;
-      const image = "";
-      return new Frase(descFrase, autor, explicacion );
+      const randomNumber = Math.floor(Math.random() * 11);
+      const image = this.asset + this.imagenes[randomNumber];
+      return new Frase( descFrase, autor, explicacion,image );
     });
+  }
+
+  cargarImagenes(){
+    this.imagenes.push("acompañado.avif");
+    this.imagenes.push("color.avif");
+    this.imagenes.push("escultura.jpg");
+    this.imagenes.push("leyendo.avif");
+    this.imagenes.push("luna.avif");
+    this.imagenes.push("marco.jpg");
+    this.imagenes.push("marco_escultura.jpg");
+    this.imagenes.push("memento_mori.png");
+    this.imagenes.push("mirando.jpg");
+    this.imagenes.push("pensando.jpg");
+    this.imagenes.push("seneca.avif");
   }
 
 }
