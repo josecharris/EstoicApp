99d167a137a002431ad4a26a187fe88f11a60875
josecharris <josepthc9781@gmail.com>
2023-08-30 17:35:03 -0500

    Se agrega contenido de epistolas morales

--- src/app/page/audiolibro/audiolibro.page.ts
+++ src/app/page/audiolibro/audiolibro.page.ts
@@ -13,6 +13,11 @@ export class AudiolibroPage implements OnInit {
   public mostrarManual: boolean = false;
   public mostrarLecciones: boolean = false;
   public mostrarEpistolaLucilio1: boolean = false;
+  public mostrarEpistolaLucilio2: boolean = false;
+  public mostrarEpistolaLucilio3: boolean = false;
+  public mostrarEpistolaLucilio4: boolean = false;
+  public mostrarEpistolaLucilio5: boolean = false;
+  public mostrarEpistolaLucilio6: boolean = false;
   public mostrarBotones: boolean = true;
   constructor() { }
 
@@ -29,6 +34,11 @@ export class AudiolibroPage implements OnInit {
         this.mostrarBotones = false;
         this.mostrarLecciones = false;
         this.mostrarEpistolaLucilio1 = false;
+        this.mostrarEpistolaLucilio2 = false;
+        this.mostrarEpistolaLucilio3 = false;
+        this.mostrarEpistolaLucilio4 = false;
+        this.mostrarEpistolaLucilio5 = false;
+        this.mostrarEpistolaLucilio6 = false;
         this.titulo = "Meditaciones";
       }
       if(numeroLibro === 2){
@@ -38,6 +48,11 @@ export class AudiolibroPage implements OnInit {
         this.mostrarBotones = false;
         this.mostrarLecciones = false;
         this.mostrarEpistolaLucilio1 = false;
+        this.mostrarEpistolaLucilio2 = false;
+        this.mostrarEpistolaLucilio3 = false;
+        this.mostrarEpistolaLucilio4 = false;
+        this.mostrarEpistolaLucilio5 = false;
+        this.mostrarEpistolaLucilio6 = false;
         this.titulo = "Manual de Epicteto";
       }
       if(numeroLibro === 3){
@@ -47,6 +62,11 @@ export class AudiolibroPage implements OnInit {
         this.mostrarBotones = false;
         this.mostrarLecciones = true;
         this.mostrarEpistolaLucilio1 = false;
+        this.mostrarEpistolaLucilio2 = false;
+        this.mostrarEpistolaLucilio3 = false;
+        this.mostrarEpistolaLucilio4 = false;
+        this.mostrarEpistolaLucilio5 = false;
+        this.mostrarEpistolaLucilio6 = false;
         this.titulo = "Lecciones de estoicismo";
       }
       if(numeroLibro === 4){
@@ -56,9 +76,83 @@ export class AudiolibroPage implements OnInit {
         this.mostrarBotones = false;
         this.mostrarLecciones = false;
         this.mostrarEpistolaLucilio1 = true;
+        this.mostrarEpistolaLucilio2 = false;
+        this.mostrarEpistolaLucilio3 = false;
+        this.mostrarEpistolaLucilio4 = false;
+        this.mostrarEpistolaLucilio5 = false;
+        this.mostrarEpistolaLucilio6 = false;
         this.titulo = "Epistolas morales a Lucilio";
       }
-      
+      if(numeroLibro === 5){
+        this.mostrarReproductor = true;
+        this.mostrarMeditaciones = false;
+        this.mostrarManual = false;
+        this.mostrarBotones = false;
+        this.mostrarLecciones = false;
+        this.mostrarEpistolaLucilio1 = false;
+        this.mostrarEpistolaLucilio2 = true;
+        this.mostrarEpistolaLucilio3 = false;
+        this.mostrarEpistolaLucilio4 = false;
+        this.mostrarEpistolaLucilio5 = false;
+        this.mostrarEpistolaLucilio6 = false;
+        this.titulo = "Epistolas morales a Lucilio";
+      }
+      if(numeroLibro === 6){
+        this.mostrarReproductor = true;
+        this.mostrarMeditaciones = false;
+        this.mostrarManual = false;
+        this.mostrarBotones = false;
+        this.mostrarLecciones = false;
+        this.mostrarEpistolaLucilio1 = false;
+        this.mostrarEpistolaLucilio2 = false;
+        this.mostrarEpistolaLucilio3 = true;
+        this.mostrarEpistolaLucilio4 = false;
+        this.mostrarEpistolaLucilio5 = false;
+        this.mostrarEpistolaLucilio6 = false;
+        this.titulo = "Epistolas morales a Lucilio";
+      }
+      if(numeroLibro === 7){
+        this.mostrarReproductor = true;
+        this.mostrarMeditaciones = false;
+        this.mostrarManual = false;
+        this.mostrarBotones = false;
+        this.mostrarLecciones = false;
+        this.mostrarEpistolaLucilio1 = false;
+        this.mostrarEpistolaLucilio2 = false;
+        this.mostrarEpistolaLucilio3 = false;
+        this.mostrarEpistolaLucilio4 = true;
+        this.mostrarEpistolaLucilio5 = false;
+        this.mostrarEpistolaLucilio6 = false;
+        this.titulo = "Epistolas morales a Lucilio";
+      }
+      if(numeroLibro === 8){
+        this.mostrarReproductor = true;
+        this.mostrarMeditaciones = false;
+        this.mostrarManual = false;
+        this.mostrarBotones = false;
+        this.mostrarLecciones = false;
+        this.mostrarEpistolaLucilio1 = false;
+        this.mostrarEpistolaLucilio2 = false;
+        this.mostrarEpistolaLucilio3 = false;
+        this.mostrarEpistolaLucilio4 = false;
+        this.mostrarEpistolaLucilio5 = true;
+        this.mostrarEpistolaLucilio6 = false;
+        this.titulo = "Epistolas morales a Lucilio";
+      }
+      if(numeroLibro === 9){
+        this.mostrarReproductor = true;
+        this.mostrarMeditaciones = false;
+        this.mostrarManual = false;
+        this.mostrarBotones = false;
+        this.mostrarLecciones = false;
+        this.mostrarEpistolaLucilio1 = false;
+        this.mostrarEpistolaLucilio2 = false;
+        this.mostrarEpistolaLucilio3 = false;
+        this.mostrarEpistolaLucilio4 = false;
+        this.mostrarEpistolaLucilio5 = false;
+        this.mostrarEpistolaLucilio6 = true;
+        this.titulo = "Epistolas morales a Lucilio";
+      }
     } else {
       this.mostrarMsjCon = true;
       this.mostrarReproductor = false;
