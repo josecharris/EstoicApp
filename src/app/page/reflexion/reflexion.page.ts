70345d4d2911f5f29488100dca841e0cee1c0805
josecharris <josepthc9781@gmail.com>
2023-09-02 16:55:32 -0500

    Se agrega contenido de material de reflexión

--- src/app/page/reflexion/reflexion.page.ts
+++ src/app/page/reflexion/reflexion.page.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReflexionPage implements OnInit {
 
+  public titulo: string = "";
+  public mostrarMsjCon: boolean = false;
+  public mostrarBotones: boolean = true;
+  public mostrarReproductor: boolean = false;
+  public mostrarReglas: boolean = false;
+  
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public mostrarMaterial(numeroLibro: number): void{
+    if (navigator.onLine) {
+      if(numeroLibro == 1){
+        this.mostrarReproductor = true;
+        this.mostrarReglas = true;
+        this.titulo = "11 reglas del estoicismo";
+      }
+    }else{
+      this.mostrarMsjCon = true;
+      this.mostrarReproductor = false;
+      this.mostrarBotones = false;
+    }
+  }
+
+  public volverBotones(): void{
+    this.mostrarBotones = true;
+    this.mostrarReproductor = false;
   }
 
 }
