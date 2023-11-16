455d4c5e83fb2bcbb96daff5c442c01267cbe405
josecharris <josepthc9781@gmail.com>
2023-11-15 20:22:22 -0500

    Se agrega documentación a la pantalla de audiolibros

--- src/app/page/conceptos/conceptos.page.ts
+++ src/app/page/conceptos/conceptos.page.ts
@@ -6,21 +6,46 @@ import { Router } from '@angular/router';
   templateUrl: './conceptos.page.html',
   styleUrls: ['./conceptos.page.scss'],
 })
+/** 
+ * <b>Descripción:</b> Componente que determina el componente para la administración
+ * de conceptos importantes<br>
+ * @autor jcharris
+*/
 export class ConceptosPage implements OnInit {
 
+  /** 
+   * <b>Descripción:</b> constructor del componente<br>
+   * @author jcharris
+   * @param router  Parámetro que determina la navegación entre componentes
+  */
   constructor( private router: Router ) { }
 
-  ngOnInit() {
-  }
+  /**
+   * <b>Descripción:</b> Determina la construcción del componente<br>
+   * @author jcharris
+  */
+  ngOnInit() { }
 
+  /**
+   * <b>Descripción:</b> Método encargado de redirigir a la pantalla de pilares<br>
+   * @author jcharris
+  */
   public navegarPilares(): void{
     this.router.navigate(['/tab-inicial/pilares']);
   }
 
+  /**
+   * <b>Descripción:</b> Método encargado de redirigir a la pantalla de mandamientos<br>
+   * @author jcharris
+  */
   public navegarMandamientos(): void{
     this.router.navigate(['/tab-inicial/mandamientos']);
   }
 
+  /**
+   * <b>Descripción:</b> Método encargado de redirigir a la pantalla de triada estoica<br>
+   * @author jcharris
+  */
   public navegarTriada(): void{
     this.router.navigate(['/tab-inicial/triada']);
   }
