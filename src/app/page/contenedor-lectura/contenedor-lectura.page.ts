fe188b7f25746aa488a1b2da75700204e0146122
josecharris <josepthc9781@gmail.com>
2023-11-15 20:32:42 -0500

    Se agrega documentación a la pantalla contenedora de lecturas

--- src/app/page/contenedor-lectura/contenedor-lectura.page.ts
+++ src/app/page/contenedor-lectura/contenedor-lectura.page.ts
@@ -8,19 +8,50 @@ import { filter } from 'rxjs/operators';
   templateUrl: './contenedor-lectura.page.html',
   styleUrls: ['./contenedor-lectura.page.scss'],
 })
+
+/** 
+ * <b>Descripción:</b> Componente que determina el componente para la administración
+ * de lecturas<br>
+ * @autor jcharris
+*/
 export class ContenedorLecturaPage implements OnInit {
 
+  /** Atributo que determina el título del libro */
   public tituloLibro: string = "";
+
+  /** Atributo que determina el nombre del archivo */
   public nombreArchivo: string = "";
+
+  /** Atributo que determina el nombre del libro */
   public nombreLibro: string = "";
+
+  /** Atributo que determina el nombre del autor del libro */
   public autorLibro: string = "";
+
+  /** Atributo que determina la descripción del libro */
   public descLibro: string = "";
+
+  /** Atributo que determina la descripción adicional */
   public descAdicional: string = "";
+
+  /** Atributo que determina la cita del autor */
   public cita: string = "";
+
+  /** Atributo que determina la cantidad */
   public cantidad = 0;
+
+  /** Atributo que determina el indicador si se muestra el visor PDF */
   public mostrarVisorPDF = false;
+
+  /** Atributo que determina la ruta del nombre del PDF*/
   public rutaPDFnombre: string = "";
 
+  /** 
+   * <b>Descripción:</b> constructor del componente<br>
+   * @author jcharris
+   * @param lecturaPasoParametrosService Servicio que determina el paso de información entre componentes
+   * @param router  Parámetro que determina la navegación entre componentes
+  */
   constructor( private lecturaPasoParametrosService: LecturaPasoParametrosService,
     private router: Router ) { 
       this.router.events
@@ -37,22 +68,30 @@ export class ContenedorLecturaPage implements OnInit {
         this.rutaPDFnombre = "/assets/files/pdf/" + this.nombreArchivo;
         this.mostrarVisorPDF = false;
       });
-    }
+  }
 
+  /**
+   * <b>Descripción:</b> Determina la construcción del componente<br>
+   * @author jcharris
+  */
   ngOnInit() {
   }
 
+  /**
+   * <b>Descripción:</b> Método encargado de redirigir a la pantalla para volver al componente
+   * que navegó al actual<br>
+   * @author jcharris
+  */
   public volver(): void{
     this.router.navigate(['/tab-inicial/lecturas']);
   }
 
-  public getRange(value: number): number[] {
-    return new Array(value);
-  }
-
+  /**
+   * <b>Descripción:</b> Método encargado de mostrar el visor PDF<br>
+   * @author jcharris
+  */
   public mostrarLibro( ): void {
     this.mostrarVisorPDF = true;
   }
-  
 
 }
